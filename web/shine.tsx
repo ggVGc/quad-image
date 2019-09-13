f61ded94b4052de7578aae884205970b0912c4e8
Chris West (Faux) <git@goeswhere.com>
2019-09-13 16:41:02 +0100

    prettier

--- web/shine.tsx
+++ web/shine.tsx
@@ -1,34 +1,44 @@
-import {h, Component, render} from 'preact';
+import { h, Component, render } from 'preact';
 
 export function init(element: HTMLElement) {
-  render(<Shine/>, element);
+  render(<Shine />, element);
 }
 
 class Shine extends Component {
   render() {
-    return <div>
-      <div id="menu">
-        <p><img id='user-button' src="user.svg" alt="user menu"/></p>
-        <div id="user-settings">
-          <div class="close">✖</div>
-          <p>Current gallery: <span id="current-gallery"><i>thinking</i></span></p>
-          <form id="user-form">
-            <label>New <abbr title="Gallery name and trigger (4-10 letter name, !, password)">gallery</abbr>:
-              <input id='new-gallery' name="gallery" required placeholder="green!battery staple"></input>
-            </label>
-            <button>sync</button>
-          </form>
+    return (
+      <div>
+        <div id="menu">
+          <p>
+            <img id="user-button" src="user.svg" alt="user menu" />
+          </p>
+          <div id="user-settings">
+            <div class="close">✖</div>
+            <p>
+              Current gallery:{' '}
+              <span id="current-gallery">
+                <i>thinking</i>
+              </span>
+            </p>
+            <form id="user-form">
+              <label>
+                New <abbr title="Gallery name and trigger (4-10 letter name, !, password)">gallery</abbr>:
+                <input id="new-gallery" name="gallery" required placeholder="green!battery staple"></input>
+              </label>
+              <button>sync</button>
+            </form>
+          </div>
+        </div>
+        <label for="realos" id="form"></label>
+        <input type="file" multiple id="realos" />
+        <ul id="images"></ul>
+        <div id="errors">Waiting for Javascript to initialise...</div>
+        <div id="tcs">
+          <p>
+            <a href="/terms/">T&amp;Cs</a>
+          </p>
         </div>
       </div>
-      <label for="realos" id="form"></label>
-      <input type="file" multiple id="realos"/>
-      <ul id="images"></ul>
-      <div id="errors">
-        Waiting for Javascript to initialise...
-      </div>
-      <div id="tcs">
-        <p><a href="/terms/">T&amp;Cs</a></p>
-      </div>
-    </div>;
+    );
   }
 }
