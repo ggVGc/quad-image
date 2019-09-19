2d171fcea4e6af4736e7febc00eea7c911f68bf9
Chris West (Faux) <git@goeswhere.com>
2019-09-19 20:51:28 +0100

    pushing things around

--- web/shine/images.tsx
+++ web/shine/images.tsx
@@ -1,6 +1,13 @@
 import { h, Component } from 'preact';
+import { SavedImage } from '../types';
 
-export class Images extends Component {
+export type MaybeImage = { code: 'image', id: SavedImage } | { code: 'loading' };
+
+export interface Props {
+  images: MaybeImage[];
+}
+
+export class Images extends Component<Props> {
   render(props) {
     return <ul id="images" />;
   }
